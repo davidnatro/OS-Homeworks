f0c6cf49cf0809523562031bed4ffb826d27b94a
David <davidnatro03@gmail.com>
2023-03-15 18:46:10 +0600

    5 final

--- hw_1/5/app/main.c
+++ hw_1/5/app/main.c
@@ -11,10 +11,9 @@
 void stringReverse(const int start, const int end, const ssize_t size, char *data);
 
 int main(int argc, char *argv[]) {
-    // Проверяем количество аргументов командной строки
     if (argc != 5) {
-        printf("Usage: %s input_file output_file start_position end_position\n", argv[0]);
-        return -1;
+        printf("%s", "Неверное количество аргументов!");
+        exit(-1);
     }
 
     // Открываем именованный канал для передачи данных между процессами
@@ -25,21 +24,21 @@ int main(int argc, char *argv[]) {
     pid_t pid1, pid2;
     pid1 = fork();
     if (pid1 < 0) {
-        printf("Unable to create process\n");
+        printf("Не удается создать процесс");
         return -1;
     }
     if (pid1 != 0) {
         // Процесс 1: чтение данных из входного файла и передача их через именованный канал
         int input_fd = open(argv[3], O_RDONLY, 0666);
         if (input_fd < 0) {
-            printf("Unable to open input file\n");
-            return -1;
+            printf("%s", "Не удается открыть файл для чтения!");
+            exit(-1);
         }
 
         int channel_fd = open(channel_name, O_WRONLY);
         if (channel_fd < 0) {
-            printf("Unable to open channel\n");
-            return -1;
+            printf("Не удается открыть канал");
+            exit(-1);
         }
 
         char buffer[BUFFER_SIZE];
@@ -50,12 +49,12 @@ int main(int argc, char *argv[]) {
         }
 
         if (close(input_fd) < 0) {
-            printf("Unable to close input file\n");
-            return -1;
+            printf("%s", "Не удается закрыть файл!");
+            exit(-1);
         }
         if (close(channel_fd)) {
-            printf("Unable to close channel\n");
-            return -1;
+            printf("Не удается закрыть канал");
+            exit(-1);
         }
     } else {
         pid2 = fork();
@@ -63,25 +62,23 @@ int main(int argc, char *argv[]) {
             // Процесс 2: обработка данных и передача результата через именованный канал
             int channel_fd = open(channel_name, O_RDONLY);
             if (channel_fd < 0) {
-                printf("Unable to open channel\n");
-                return -1;
+                printf("Не удается открыть канал");
+                exit(-1);
             }
 
             char buffer[BUFFER_SIZE];
             ssize_t bytes_read = read(channel_fd, buffer, BUFFER_SIZE);
 
-            // Читаем данные из именованного канала и переворачиваем их в соответствии с заданными позициями
             int start_pos = atoi(argv[1]);
             int end_pos = atoi(argv[2]);
 
             stringReverse(start_pos, end_pos, bytes_read, buffer);
 
-            // Передаем результат обработки через именованный канал
             write(channel_fd, buffer, bytes_read);
 
             int close_res = close(channel_fd);
             if (close_res == -1) {
-                printf("Unable to close channel\n");
+                printf("Не удается закрыть канал");
                 exit(-1);
             }
 
@@ -89,13 +86,13 @@ int main(int argc, char *argv[]) {
             if (pid3 == 0) {
                 int output_fd = open(argv[4], O_WRONLY | O_CREAT, S_IWOTH | S_IWUSR | 0644);
                 if (output_fd < 0) {
-                    perror("Unable to open output file\n");
-                    return -1;
+                    printf("%s", "Не удается открыть файл для записи!");
+                    exit(-1);
                 }
 
                 write(output_fd, buffer, bytes_read);
                 if (close(output_fd) == -1) {
-                    printf("Unable to close output file\n");
+                    printf("%s", "Не удается закрыть файл для записи!");
                     exit(-1);
                 }
             }
