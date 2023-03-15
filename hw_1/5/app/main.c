e821d17eb6f13c1909ea52c9bfdc7f2100e51872
David <davidnatro03@gmail.com>
2023-03-15 18:25:13 +0600

    outputs

--- hw_1/5/app/main.c
+++ hw_1/5/app/main.c
@@ -24,9 +24,13 @@ int main(int argc, char *argv[]) {
     // Создаем процессы для чтения, обработки и записи данных
     pid_t pid1, pid2;
     pid1 = fork();
+    if (pid1 < 0) {
+        printf("Unable to create process\n");
+        return -1;
+    }
     if (pid1 != 0) {
         // Процесс 1: чтение данных из входного файла и передача их через именованный канал
-        int input_fd = open(argv[3], O_RDONLY);
+        int input_fd = open(argv[3], O_RDONLY, 0666);
         if (input_fd < 0) {
             printf("Unable to open input file\n");
             return -1;
@@ -45,9 +49,14 @@ int main(int argc, char *argv[]) {
             write(channel_fd, buffer, bytes_read);
         }
 
-        close(input_fd);
-        close(channel_fd);
-        exit(0);
+        if (close(input_fd) < 0) {
+            printf("Unable to close input file\n");
+            return -1;
+        }
+        if (close(channel_fd)) {
+            printf("Unable to close channel\n");
+            return -1;
+        }
     } else {
         pid2 = fork();
         if (pid2 == 0) {
@@ -68,13 +77,7 @@ int main(int argc, char *argv[]) {
             stringReverse(start_pos, end_pos, bytes_read, buffer);
 
             // Передаем результат обработки через именованный канал
-            int channel2_fd = open(channel_name, O_WRONLY);
-            if (channel2_fd < 0) {
-                printf("Unable to open channel\n");
-                return -1;
-            }
-
-            write(channel2_fd, buffer, bytes_read);
+            write(channel_fd, buffer, bytes_read);
 
             int close_res = close(channel_fd);
             if (close_res == -1) {
@@ -82,30 +85,24 @@ int main(int argc, char *argv[]) {
                 exit(-1);
             }
 
-            close_res = close(channel2_fd);
-            if (close_res == -1) {
-                printf("Unable to close channel\n");
-                exit(-1);
-            }
-
             pid_t pid3 = fork();
             if (pid3 == 0) {
-                int output_fd = open(argv[4], O_WRONLY | O_CREAT, 0666);
+                int output_fd = open(argv[4], O_WRONLY | O_CREAT, S_IWOTH | S_IWUSR | 0644);
                 if (output_fd < 0) {
-                    printf("Unable to open input file\n");
+                    perror("Unable to open output file\n");
                     return -1;
                 }
+
                 write(output_fd, buffer, bytes_read);
                 if (close(output_fd) == -1) {
                     printf("Unable to close output file\n");
                     exit(-1);
                 }
-                exit(0);
-            } else {
-                exit(0);
             }
         }
     }
+
+    while (wait(NULL) > 0) {}
 
     return 0;
 }
